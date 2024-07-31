ca7bddde3a2b911e574f8dc21f334760a9e7093e
K-Nura <kitayev.n@gmail.com>
2024-08-01 01:42:45 +0500

    day-3

---
+++ src/modules/Main.jsx
@@ -0,0 +1,11 @@
+import { Products } from './Froducts';
+import { Promo } from './Promo';
+
+export const Main = () => {
+	return (
+		<main className="main">
+			<Promo />
+			<Products />
+		</main>
+	);
+};
